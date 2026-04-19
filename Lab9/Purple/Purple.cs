ba8452002804d4801eede44bc268f5e488a4849b
wodo <96520106+wodocanal@users.noreply.github.com>
2026-04-19 22:14:44 +0300

    Update Purple.cs

--- Lab9/Purple/Purple.cs
+++ Lab9/Purple/Purple.cs
@@ -1,3 +1,16 @@
 namespace Lab9.Purple
 {
+    public abstract class Purple
+    {
+        protected string _input;
+        public string Input => _input;
+        public string Output { get; protected set; }
+        protected Purple(string text) { _input = text; }
+        public abstract void Review();
+        public void ChangeText(string text)
+        {
+            _input = text;
+            Review();
+        }
+    }
 }
